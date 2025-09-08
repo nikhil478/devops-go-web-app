bd0568a83d39b1d8af7be8d2e41e0dbe77bcd56e
nikhil478 <nikhilmatta45@gmail.com>
2025-09-08 08:50:28 +0530

    create simple http server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+
+func main(){
+	
+	handler := http.NewServeMux()
+
+	handler.HandleFunc("/hello", func(w http.ResponseWriter,r *http.Request){
+		w.WriteHeader(200)
+		w.Write([]byte("Hello World !"))
+	})
+
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		log.Fatal(err)
+	}
+}
