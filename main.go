0f38676645a95f49ffa629417f295d519726b4d0
nikhil478 <nikhilmatta45@gmail.com>
2025-09-18 03:53:37 +0530

    some changes

--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main(){
 	handler := http.NewServeMux()
 
 	handler.HandleFunc("/hello", func(w http.ResponseWriter,r *http.Request){
-		
+
 		w.WriteHeader(200)
 
-		w.Write([]byte("Hello World !"))
+		w.Write([]byte("Hola World !"))
 	})
 
 	if err := http.ListenAndServe(":8080", handler); err != nil {
